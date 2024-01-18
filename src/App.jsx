6c2a420641fd208d87626f17640ec9b3f20e8775
toasterboaster <107941335+toasterboaster@users.noreply.github.com>
2024-01-18 20:27:52 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import Start from './Start.jsx'
+import React from 'react'
+
+export default function App() {
+  return (
+    <main>
+      React ⚛️ + Vite ⚡ + Replit
+      <Start />
+    </main>
+  )
+}
---
+++ src/RecipePage.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function RecipePage(){
+  return (
+    <div>
+    </div>
+  )
+}

197992c35c500ed4ebea0bd3db4f7365d94ac98d
Josh <willJosh44@yahoo.com>
2024-02-09 21:26:04 +0000

    worked on start page; still need to work on styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
-import './App.css'
-import Start from './Start.jsx'
-import React from 'react'
+import "./App.css";
+import Start from "./Start.jsx";
+import React from "react";
 
 export default function App() {
+  const [inputValue, setInputValue] = React.useState("");
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+    console.log(inputValue);
+  };
+
   return (
     <main>
-      React ⚛️ + Vite ⚡ + Replit
-      <Start />
+      <Start 
+        className="start"
+        handleInputChange={handleInputChange} />
     </main>
-  )
+  );
 }
--- src/Start.jsx
+++ src/Start.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from "react";
+import './App.css'
 
-export default function Start(){
+export default function Start(props) {
   return (
-    <div>
-      Hi
+    <div className={props.className}>
+      <h2>Title here</h2>
+      <input
+        type="text"
+        placeholder="Enter recipe"
+        onChange={props.handleInputChange}
+      ></input>
+      <button>Go</button>
     </div>
-  )
+  );
 }

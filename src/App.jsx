8f033dab7b3f949ac533dbcee7c0f239f513de15
toasterboaster <107941335+toasterboaster@users.noreply.github.com>
2024-12-17 21:10:21 +0000

    12/17 update

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,36 @@
 import "./App.css";
 import Start from "./Start.jsx";
 import React from "react";
+import RecipeMenu from "./RecipeMenu.jsx";
 
 export default function App() {
   const [inputValue, setInputValue] = React.useState("");
+  const [start, setStart] = React.useState(false);
+  const [data, setData] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=d34d7c6bb49a4378b897013b266052e0&query=${inputValue}`)
+      .then((response) => response.json())
+      .then((json) => setData(json))
+      .catch((error) => console.error(error));
+  }, [inputValue]);
 
   const handleInputChange = (e) => {
     setInputValue(e.target.value);
     console.log(inputValue);
   };
 
+  function startApp() {
+    setStart((prev) => !prev);
+    console.log(data.results)
+  }
+
   return (
-    <main>
-      <Start 
-        className="start"
-        handleInputChange={handleInputChange} />
+    <main className="main">
+      {!start && (
+        <Start handleInputChange={handleInputChange} start={startApp} />
+      )}
+      {start && <RecipeMenu recipes={data}/>}
     </main>
   );
 }
--- src/RecipeMenu.jsx
+++ src/RecipeMenu.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import React from "react";
 
-export default function RecipeMenu(){
+export default function RecipeMenu(props) {
+  const [displayCount, setDisplayCount] = React.useState(3);
+
+  const loadMore = () => {
+    setDisplayCount(displayCount + 3);
+  }
+  
   return (
     <div>
+      <div className={"recipeMenuCardContainer"}>
+        {props.recipes.results.slice(0,displayCount).map((recipe, index) => (
+          <div key={index} className={"recipeCard"}>
+            <div>
+              <h3 className={'recipeCardText'}>{recipe.title}</h3>
+            </div>
+            <img className={'recipeCardImage'} src={recipe.image}/>
+          </div>
+        ))}
+      </div>
+        <div className='loadMoreButtonContainer'>
+          <button className='loadMoreButton' onClick={loadMore}>Load More</button>
+        </div>
     </div>
-  )
+    /*
+    <div>
+      <div>This will be a recipe card</div>
+      <div>This will be a recipe card</div>
+      <div>This will be a recipe card</div>
+    </div>*/
+  );
 }
